822623116b9128556abe59002ef8f506df9cc31c
miker7790 <48162515+miker7790@users.noreply.github.com>
2020-12-02 15:14:13 -0500

    Add files via upload

---
+++ Hangman-D/manager/games/tictactoe.py
@@ -0,0 +1,12 @@
+class TICTACTOE:
+    
+    def __init__(self):
+        self.game={}             
+        
+
+    # sets rules/ initalizes game     
+    def playGame(self):
+        
+        print("PLAYING TICTACTOE")
+        self.game['win'] = True
+        return (self.game)
